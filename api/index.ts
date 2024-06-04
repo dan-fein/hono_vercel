f9570da184c71e43dd630a05d6e8d34640a805de
Dan Fein <1616865+dan-fein@users.noreply.github.com>
2024-06-04 11:36:09 -0400

    latest

--- api/index.ts
+++ api/index.ts
@@ -1,14 +1,14 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 
-export const config = {
-  runtime: 'edge'
-}
+// export const config = {
+//   runtime: 'edge'
+// }
 
 const app = new Hono().basePath('/api')
 
 app.get('/', (c) => {
-  return c.json({ message: 'Hello Hono!' })
+  return c.json({ message: "Congrats! You've deployed Hono to Vercel" })
 })
 
 export default handle(app)
