f606647fa0cfb310d8a476e9431f8819ff78e706
Gal Schlezinger <gal@spitfire.co.il>
2024-06-04 18:37:21 +0200

    Use exported methods to use the Web API handlers

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,16 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
 
-const app = new Hono().basePath('/api')
+const app = new Hono().basePath("/api");
 
-app.get('/', (c) => {
-  return c.json({ message: "Congrats! You've deployed Hono to Vercel" })
-})
+app.get("/", (c) => {
+  return c.json({ message: "Congrats! You've deployed Hono to Vercel" });
+});
 
-export default handle(app)
+const handler = handle(app);
+
+export const GET = handler;
+export const POST = handler;
+export const PATCH = handler;
+export const PUT = handler;
+export const OPTIONS = handler;
